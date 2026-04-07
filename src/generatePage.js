615848a2f4af24c7203b3583dc4c949bbd2d1bfe
lpfader <lpfader@gmail.com>
2026-04-06 20:21:47 -0400

    Initial working Node build

---
+++ src/generatePage.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>My Node Page</title>
+</head>
+<body>
+  <h1>Welcome to my Node.js page</h1>
+  <p>This page was generated using Node.js.</p>
+</body>
+</html>
+`;
+
+fs.writeFileSync("public/index.html", html);
+console.log("Generated public/index.html");
